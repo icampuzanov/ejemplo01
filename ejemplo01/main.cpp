0cb717d2aea882e12365b742d232e88480dc9014
IgnacioCampuzano <ignaciocampuzano4334@gmail.com>
2021-09-09 10:25:08 -0400

    Datos personales rellenados

--- ejemplo01/main.cpp
+++ ejemplo01/main.cpp
@@ -2,6 +2,11 @@
 
 int main() {
 	std::cout << "Datos personales:" << std::endl;
+	std::cout << "Nombre: Campuzano Villarroel Ignacio" << std::endl;
+	std::cout << "Carrera: Ing. de Sistemas" << std::endl;
+	std::cout << "CI: 10348872" << std::endl;
+	std::cout << "CU: 35-5147" << std::endl;
+
 
 	return 0;
 }
